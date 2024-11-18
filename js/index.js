f656dbbec5bb801ce0864997af2925dc1a68e9ed
huiyun-1009 <huiyun95@gmail.com>
2024-11-19 02:46:59 +0900

    반응형

--- js/index.js
+++ js/index.js
@@ -58,56 +58,112 @@ $(function () {
             console.log("con1입니다.");
             $(".idphoto").addClass("on");
             $(".about li").addClass("on");
+            $(" #con1 .about p").each(function (index) {
+                $(this)
+                    .delay(index * 300)
+                    .queue(function (next) {
+                        $(this).addClass("on");
+                        next();
+                    });
+            });
+
+            if (scroll > con2 && scroll <= con3) {
+                $("header").addClass("on");
+                $("#con2 li .project").addClass("on");
+                $("#con2 li .mokup").addClass("on");
+            } else {
+                $("header").removeClass("on");
+                $("#con2 li .project").removeClass("on");
+                $("#con2 li .mokup").removeClass("on");
+            }
         }
 
-        if (scroll > con2 && scroll <= con3) {
-            $("header").addClass("on");
-            $("#con2 li .project").addClass("on");
-            $("#con2 li .mokup").addClass("on");
-        } else {
-            $("header").removeClass("on");
-            $("#con2 li .project").removeClass("on");
-            $("#con2 li .mokup").removeClass("on");
-        }
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: "#con3 .ilust",
+                start: "top 90%",
+                end: "20% 100%",
+                scrub: 2,
+                markers: true,
+            },
+        })
+            .to(
+                "#con3 li:nth-child(1)",
+                { y: "-400px", duration: 1, ease: "none" },
+                0.2
+            )
+            .to(
+                "#con3 li:nth-child(2)",
+                { y: "-400px", duration: 1, ease: "none" },
+                0.4
+            )
+            .to(
+                "#con3 li:nth-child(3)",
+                { y: "-400px", duration: 1, ease: "none" },
+                0.6
+            )
+            .to(
+                "#con3 li:nth-child(4)",
+                { y: "-400px", duration: 1, ease: "none" },
+                0.8
+            )
+            .to(
+                "#con3 li:nth-child(5)",
+                { y: "-400px", duration: 1, ease: "none" },
+                1
+            )
+            .to(
+                "#con3 li:nth-child(6)",
+                { y: "-400px", duration: 1, ease: "none" },
+                1.2
+            );
     });
-
-    gsap.timeline({
-        scrollTrigger: {
-            trigger: "#con3 .ilust",
-            start: "top 90%",
-            end: "20% 100%",
-            scrub: 2,
-            markers: true,
-        },
-    })
-        .to(
-            "#con3 li:nth-child(1)",
-            { y: "-400px", duration: 1, ease: "none" },
-            0.2
-        )
-        .to(
-            "#con3 li:nth-child(2)",
-            { y: "-400px", duration: 1, ease: "none" },
-            0.4
-        )
-        .to(
-            "#con3 li:nth-child(3)",
-            { y: "-400px", duration: 1, ease: "none" },
-            0.6
-        )
-        .to(
-            "#con3 li:nth-child(4)",
-            { y: "-400px", duration: 1, ease: "none" },
-            0.8
-        )
-        .to(
-            "#con3 li:nth-child(5)",
-            { y: "-400px", duration: 1, ease: "none" },
-            1
-        )
-        .to(
-            "#con3 li:nth-child(6)",
-            { y: "-400px", duration: 1, ease: "none" },
-            1.2
-        );
 });
+
+// gsap.timeline({
+//     scrollTrigger: {
+//         trigger: "#con2",
+//         start: "bottom 90%",
+//         end: "20% 100%",
+//         scrub: 2,
+//         markers: true,
+//         pin: "#con2",
+//     },
+// });
+
+// gsap.timeline({
+//     scrollTrigger: {
+//         trigger: "#con2 .portfolio",
+//         start: "center center",
+//         end: "20% 100%",
+//         scrub: 2,
+//         markers: true,
+//         pin: "#con2 li",
+//     },
+// });
+
+// let panels = gsap.utils.toArray("#con2 .portfolio li");
+// //선택자
+// panels.forEach((panel, i) => {
+//     ScrollTrigger.create({
+//         trigger: panel,
+//         start: "top top",
+//         end: "bottom top",
+//         pin: "#con2",
+//         scrub: 0.5,
+//         pinSpacing: false,
+//     });
+// });
+// ScrollTrigger.create({
+//     trigger: "#con2",
+//     start: "top top",
+//     end: "bottom 150px",
+//     pin: "#con2 .portfolio li:nth-child(1)",
+// });
+
+// ScrollTrigger.create({
+//     trigger: "#con2",
+//     start: "top center",
+//     end: "+=200", // 200px past the start
+//     pin: "#con2 .portfolio li:nth-child(2)",
+// });
